3e1692aba53580b976f287a696851bab29a78e2b
cookiemorim <128754636+cookiemorim@users.noreply.github.com>
2024-05-23 00:33:43 +0000

    22/05/24 - Seeder

---
+++ modelo/database/seeders/ProdutoSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProdutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('produtos')->insert([
+            'nome' => 'Bolacha',
+            'quantidade' => 2,
+            'preco' => 5.00
+        ]);
+    }
+}
