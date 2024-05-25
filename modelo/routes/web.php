c7a1340a3339ecaedf14ae6aa971e29cba777b62
cookiemorim <128754636+cookiemorim@users.noreply.github.com>
2024-05-25 00:17:38 +0000

    24/05/24-LP

--- modelo/routes/web.php
+++ modelo/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('nova');
 });
 
 Route::get('/dashboard', function () {
@@ -18,3 +18,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
