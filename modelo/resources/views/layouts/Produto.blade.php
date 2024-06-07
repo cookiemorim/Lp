2c3ed4899888315fc9ee898c3e3f838151ef5bd7
cookiemorim <128754636+cookiemorim@users.noreply.github.com>
2024-06-07 00:13:29 +0000

    06/06/24

--- modelo/resources/views/layouts/Produto.blade.php
+++ modelo/resources/views/layouts/Produto.blade.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    <p> Teste </p>
+    <form action="{{route('produto.store')}}" method="POST">
+        @csrf
+
+        <label>Nome: </label>
+        <input type="text" name="nome" id="nome" placeholder="Nome do produto" required><br><br>
+
+        <label>Valor: </label>
+        <input type="text" name="valor" id="valor" placeholder="Usar '.' separar real do centavo" required><br><br>
+
+        <label>descrição: </label>
+        <input type="date" name="vencimento" id="vencimento"  required><br><br>
+
+        <button type="submit">Cadastrar</button>
+
+    </form>
+
+    
 </body>
 </html>
